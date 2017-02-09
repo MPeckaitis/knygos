be58bf982d23b3191c56bac623f1af26d5ed86a8
MPeckaitis <peckaitis.martynas@gmail.com>
2017-02-09 09:22:10 +0200

    pridedam charset kad rodytu lietuviskas raides

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 $connect = mysqli_connect("127.0.0.1","root","","knygos");
+mysqli_set_charset($connect, "utf8");
 if (mysqli_connect_errno()){
 	die ("Nepavyko prisijunti prie duomenų bazės".mysqli_connect_errno());
 }
