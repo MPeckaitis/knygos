96f93dd667dbca0f2002d88bcca9fa9cfed160bd
MPeckaitis <peckaitis.martynas@gmail.com>
2017-02-09 09:53:06 +0200

    gaunam info is db pagal kintamaji id

--- knyga.php
+++ knyga.php
@@ -2,7 +2,18 @@
 
 include ("include/connect.php");
 if (array_key_exists("id", $_GET)){
-	echo $_GET["id"];
+	$query = "SELECT * FROM `knygos` WHERE id = '".$_GET['id']."' LIMIT 1";
+	$runQuery = mysqli_query($connect, $query);
+	$result = mysqli_fetch_array($runQuery);
+	$pavadinimas = $result["pavadinimas"];
+	$autorius = $result["autorius"];
+	$metai = $result["metai"];
+	$zanras = $result["zanras"];
+	$info = $result["informacija"];
+	$foto = $result["foto"];
+	echo $pavadinimas;
+} else {
+	header ("Location: index.php");
 }
 
 ?>
