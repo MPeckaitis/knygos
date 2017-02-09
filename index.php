cf9de6c48a2a56057daa6f425a2b5f3b10387d86
MPeckaitis <peckaitis.martynas@gmail.com>
2017-02-09 09:44:02 +0200

    prisijungimo prie duomenu bazes info atskirame faile
    kadangi is kito puslapio taip pat jungsimes prie db kuriame atskira
    faila kad nekartoti to pacio kodo

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
-$connect = mysqli_connect("127.0.0.1","root","","knygos");
-mysqli_set_charset($connect, "utf8");
-if (mysqli_connect_errno()){
-	die ("Nepavyko prisijunti prie duomenų bazės".mysqli_connect_errno());
-}
+include ("include/connect.php");
 
 ?>
 
